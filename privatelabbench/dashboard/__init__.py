9fbf3c6bd00735cf14ba0decc95982bfb0e0c075
Dipesh Babu <59379458+dipeshbabu@users.noreply.github.com>
2026-04-28 18:22:23 -0400

    Add dashboard package

---
+++ privatelabbench/dashboard/__init__.py
@@ -0,0 +1,5 @@
+"""Hosted-dashboard primitives for sanitized PrivateLabBench result sync."""
+
+from privatelabbench.dashboard.schemas import SanitizedRunPayload
+
+__all__ = ["SanitizedRunPayload"]
